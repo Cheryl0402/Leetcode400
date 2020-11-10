0c0ccff47f535fb2218063030c280c64822f1197
Cheryl0402 <yi.cheryl.chen@gmail.com>
2020-11-10 14:38:04 -0800

    add brute force solution

---
+++ SolutionContainerWithMostWater.java
@@ -0,0 +1,12 @@
+class SolutionContainerWithMostWater {
+    public int maxArea(int[] height) {
+        
+        int max = 0;
+        for(int i = 0; i < height.length; i++){            
+            for(int j = i + 1; j < height.length; j++){                
+                max =Math.max(max, (j - i) * Math.min(height[i], height[j])); 
+            }
+        }
+        return max;
+    }
+}
